0103080394878abad2c76f4a7d52ac6c5de88da5
Owen Healy <owen@pulsetor.com>
2014-10-15 15:31:20 -0400

    restartOnError()

--- src/com/github/ellbur/lapper/Lapper.scala
+++ src/com/github/ellbur/lapper/Lapper.scala
@@ -13,7 +13,7 @@ object Lapper {
 
   val done = Return(())
 
-  def actor(ff: Reactable => Next) = {
+  def actor(ff: Reactable => Next): Actor = {
     val inbox = new Inbox
 
     new Thread {
@@ -25,4 +25,19 @@ object Lapper {
 
     inbox: Receiver
   }
+
+  type Actor = Receiver
+
+  def restartOnError(f: Next): Next =
+    try {
+      trampoline(f)
+      done
+    }
+    catch {
+      case t: Throwable =>
+        System.err.println("Actor caught exception:")
+        t.printStackTrace()
+        System.err.println("Inside restartOnError -- Actor will now restart.")
+        restartOnError(f)
+    }
 }
--- src/com/github/ellbur/lapper/Reactable.scala
+++ src/com/github/ellbur/lapper/Reactable.scala
@@ -1,8 +1,11 @@
 
 package com.github.ellbur.lapper
 import scala.concurrent.duration.Duration
+import Lapper._
 
 trait Reactable {
   def react[T](f: PartialFunction[Any, Continuation[T]]): Continuation[T]
   def reactWithin[T](timeout: Duration)(f: PartialFunction[Option[Any], Continuation[T]]): Continuation[T]
+
+  def restartOnError(f: Next): Next = Lapper.restartOnError(f)
 }
